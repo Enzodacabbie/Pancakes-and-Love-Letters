50f1a348b1662a25a152eced53eb974cce96fa22
Enzodacabbie <lvcabrera@cpp.edu>
2023-03-12 20:18:28 -0700

    simplified the camera

--- Pancakes and Love Letters/Assets/Scripts/Camera/ThirdPersonCam.cs
+++ Pancakes and Love Letters/Assets/Scripts/Camera/ThirdPersonCam.cs
@@ -2,31 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCam : MonoBehaviour{
+public class ThirdPersonCam : MonoBehaviour
+{
     [Header("References")]
     public Transform orientation;
     public Transform player;
     public Transform model;
     public Rigidbody rb;
 
-    public float rotationSpeed;
+    [SerializeField] float rotationSpeed;
 
-    void Start(){
+    private void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    void Update(){
+    void Update()
+    {
         //rotate orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
         //rotate player object
         float horizontalInput = Input.GetAxis("Horizontal");
         float vertialInput = Input.GetAxis("Vertical");
+
         Vector3 inputDir = orientation.forward * vertialInput + orientation.right * horizontalInput;
 
-        if(inputDir != Vector3.zero){
+
+        if (inputDir != Vector3.zero){
             model.forward = Vector3.Slerp(model.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
     }
